746c158397d7b777f14f53ff0f03f3a87e6a5b55
Joshua DiChello <48339347+SwizzleZticks@users.noreply.github.com>
2025-01-01 12:10:26 -0500

    add required and fix localid from localunion to short

--- Models/User.cs
+++ Models/User.cs
@@ -7,24 +7,23 @@ public partial class User
 {
     public ushort Id { get; set; }
 
-    public string Username { get; set; } = null!;
+    public required string Username { get; set; } = null!;
 
-    public string PasswordHash { get; set; } = null!;
+    public required string PasswordHash { get; set; } = null!;
 
-    public string FirstName { get; set; } = null!;
+    public required string FirstName { get; set; } = null!;
 
-    public string LastName { get; set; } = null!;
+    public required string LastName { get; set; } = null!;
 
-    public string Email { get; set; } = null!;
+    public required string Email { get; set; } = null!;
 
-    public short? LocalId { get; set; }
+    public required short LocalId { get; set; }
 
-    public bool IsVerified { get; set; }
+    public required bool IsVerified { get; set; }
 
-    public bool IsAdmin { get; set; }
+    public required bool IsAdmin { get; set; }
 
-    public bool IsBlacklisted { get; set; }
+    public required bool IsBlacklisted { get; set; }
 
-    public virtual LocalUnion? Local { get; set; }
-    
+    public virtual LocalUnion Local { get; set; } = null!;
 }
