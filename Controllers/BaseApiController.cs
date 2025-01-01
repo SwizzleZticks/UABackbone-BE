26ff40ac9961b1d6670edccefb8d70b6421f726f
Joshua DiChello <48339347+SwizzleZticks@users.noreply.github.com>
2025-01-01 12:08:12 -0500

    create base controller to remove repetitive code

---
+++ Controllers/BaseApiController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace UABackbone_Backend.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class BaseApiController : ControllerBase
+{
+    
+}
