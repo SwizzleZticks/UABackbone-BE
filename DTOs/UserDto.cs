bbd146223c442aa0e1c9202c86f4f14f39954aa5
Joshua DiChello <48339347+SwizzleZticks@users.noreply.github.com>
2025-01-03 13:23:40 -0500

    add user dto to hide password

---
+++ DTOs/UserDto.cs
@@ -0,0 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace UABackbone_Backend.DTOs;
+
+public class UserDto
+{
+    [Required]    
+    public required string Username { get; set; } = null!;
+    [Required]
+    public required string FirstName { get; set; } = null!;
+    [Required]
+    public required string LastName { get; set; } = null!;
+    [Required]
+    public required string Email { get; set; } = null!;
+    [Required]
+    public short Local { get; set; }
+}
