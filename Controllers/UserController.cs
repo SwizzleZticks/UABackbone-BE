8d85e1a1b6d9c8651c61996aad55ee31349baeca
Joshua DiChello <48339347+SwizzleZticks@users.noreply.github.com>
2025-01-04 14:38:05 -0500

    add basic endpoints for user controller

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -5,18 +5,48 @@ using UABackbone_Backend.Models;
 namespace UABackbone_Backend.Controllers;
 public class UserController(RailwayContext context) : BaseApiController
 {
-    //Get all users
     [HttpGet]
     [ProducesResponseType(StatusCodes.Status200OK)]
     public async Task<ActionResult<LocalUnion>> GetUsersAsync()
     {
         return Ok(await context.Users.ToListAsync());
     }
-    //Get user by ID
-    
-    //Update user
-    
-    //Create user
-    
-    //Delete user
+
+    [HttpGet("{id}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<ActionResult<User>> GetUserByIdAsync(int id)
+    {
+        var user = await context.Users.FindAsync(id);
+        return user != null ? Ok(user) : NotFound();
+    }
+
+    [HttpPut("{id}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<ActionResult<LocalUnion>> UpdateUserAsync([FromBody] User aUser, ushort id)
+    {
+        aUser.Id = id;
+        context.Update(aUser);
+        await context.SaveChangesAsync();
+
+        return Ok(aUser);
+    }
+
+    [HttpDelete("{id}")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<ActionResult<User>> DeleteUserAsync(int id)
+    {
+        var user = await context.Users.FindAsync(id);
+
+        if (user == null)
+        {
+            return NotFound("User not found");
+        }
+        context.Users.Remove(user);
+        await context.SaveChangesAsync();
+        
+        return NoContent();
+    }
 }
