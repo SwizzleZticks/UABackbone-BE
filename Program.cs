941ff36d0f0028bef8f0c604bbf53b8eb9236aa2
Joshua DiChello <48339347+SwizzleZticks@users.noreply.github.com>
2025-01-18 10:19:38 -0500

    maybe this?

--- Program.cs
+++ Program.cs
@@ -24,7 +24,7 @@ namespace UABackbone_Backend
                 options.AddDefaultPolicy(
                     policy =>
                     {
-                        policy.WithOrigins("http://localhost:8080", "https://uabackbone-be.up.railway.app")
+                        policy.WithOrigins("http://localhost:8080", "https://uabackbone-be.up.railway.app:55014")
                             .AllowAnyMethod()
                             .AllowAnyHeader()
                             .AllowAnyOrigin();
