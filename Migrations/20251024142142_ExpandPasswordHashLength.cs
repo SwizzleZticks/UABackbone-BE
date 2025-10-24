5d50bafd490b6f50ae17299eb8b320ab4cd749cb
SwizzleZticks <jddichello@gmail.com>
2025-10-24 10:26:35 -0400

    fix pw hash in db

---
+++ Migrations/20251024142142_ExpandPasswordHashLength.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UABackbone_Backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class ExpandPasswordHashLength : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "password_hash",
+                table: "Users",
+                type: "varchar(255)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(60)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "password_hash",
+                table: "Users",
+                type: "varchar(60)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(255)");
+        }
+    }
+}
