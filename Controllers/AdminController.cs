8821e4ae1eeb96df5745434c6d7878454861ad9d
SwizzleZticks <jddichello@gmail.com>
2026-09-06 12:25:47 -0400

    move endpoints to admin for locals

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -200,6 +200,65 @@ public class AdminController(RailwayContext context, IEmailService emailService,
         return NoContent();
     }
 
+    [HttpPost]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<ActionResult<LocalUnion>> CreateLocalAsync([FromBody] LocalUnion newLocal)
+    {
+        context.LocalUnions.Add(newLocal);
+        await context.SaveChangesAsync();
+
+        return Created("api/Locals", newLocal);
+    }
+
+
+    [HttpPatch("{local}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<ActionResult<LocalUnion>> UpdateLocalAsync(int local, [FromBody] LocalUnionDto aLocal)
+    {
+        var queriedLocal = await context.LocalUnions.FindAsync(local);
+        if (queriedLocal == null)
+        {
+            return NotFound("Local not found");
+        }
+
+        var dtoProperties = typeof(LocalUnionDto).GetProperties();
+        foreach (var prop in dtoProperties)
+        {
+            var value = prop.GetValue(aLocal);
+            if (value != null)
+            {
+                var entityProperty = queriedLocal.GetType().GetProperty(prop.Name);
+                if (entityProperty != null)
+                {
+                    entityProperty.SetValue(queriedLocal, value);
+                }
+            }
+        }
+        await context.SaveChangesAsync();
+
+        return Ok(queriedLocal);
+    }
+
+    [HttpDelete("{local}")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<ActionResult> DeleteLocalAsync(int local)
+    {
+        var localUnion = await context.LocalUnions.FindAsync(local);
+
+        if (localUnion == null)
+        {
+            return NotFound("Local not found");
+        }
+
+        context.LocalUnions.Remove(localUnion);
+        await context.SaveChangesAsync();
+
+        return NoContent();
+    }
+
     [HttpGet("dashboard-data")]
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status401Unauthorized)]
