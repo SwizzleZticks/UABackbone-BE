937697db6a3352495774e86ba7928d5dd8cdf358
SwizzleZticks <jddichello@gmail.com>
2026-09-26 11:21:06 -0400

    dont worry im actually fixing it this time

---
+++ Migrations/20260926152035_UpdateAdminActionDeleteBehavior.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UABackbone_Backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateAdminActionDeleteBehavior : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AdminActions_Users_UserAffectedId",
+                table: "AdminActions");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AdminActions_Users_UserAffectedId",
+                table: "AdminActions",
+                column: "UserAffectedId",
+                principalTable: "Users",
+                principalColumn: "id",
+                onDelete: ReferentialAction.SetNull);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AdminActions_Users_UserAffectedId",
+                table: "AdminActions");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AdminActions_Users_UserAffectedId",
+                table: "AdminActions",
+                column: "UserAffectedId",
+                principalTable: "Users",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
